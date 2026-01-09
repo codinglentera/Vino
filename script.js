4d39c73ee9b9339fa66c0d1c3aed3cd69dcaa3f3
coding lentera <informatikasmplentera2526@gmail.com>
2026-01-09 10:11:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let uang = 5000;
+let isPlaying = false;
 
+function mulaiBilling() {
+    if (!isPlaying) {
+        isPlaying = true;
+        document.getElementById("status").innerText = "Bermain...";
+        
+        // Simulasi pelanggan main selama 3 detik
+        setTimeout(() => {
+            uang += 2000;
+            document.getElementById("money").innerText = uang;
+            document.getElementById("status").innerText = "Kosong";
+            isPlaying = false;
+            alert("Pelanggan selesai! Anda dapat Rp 2.000");
+        }, 3000);
+    }
+}
