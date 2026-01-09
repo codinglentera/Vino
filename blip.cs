df1170b53ee37ef6c5f183a81aae45a50fbd2331
coding lentera <informatikasmplentera2526@gmail.com>
2026-01-09 10:34:55 +0800

    Update blip.cs

--- blip.cs
+++ blip.cs
@@ -1,17 +1,1 @@
-public class BillingSystem : MonoBehaviour {
-    public float hargaPerJam = 5000f;
-    private float startTime;
-    private bool isOccupied;
 
-    public void MulaiBermain() {
-        startTime = Time.time;
-        isOccupied = true;
-    }
-
-    public void SelesaiBermain() {
-        float durasi = (Time.time - startTime) / 3600f; // Konversi ke jam
-        int totalBayar = Mathf.CeilToInt(durasi * hargaPerJam);
-        EkonomiManager.Instance.TambahUang(totalBayar);
-        isOccupied = false;
-    }
-}
