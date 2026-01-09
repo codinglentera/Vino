ee6bace2faa4a36249740f910b0eae2bbb999eba
coding lentera <informatikasmplentera2526@gmail.com>
2026-01-09 10:06:20 +0800

    Update blop.cs

--- blop.cs
+++ blop.cs
@@ -1,1 +1,18 @@
+// Contoh skrip sederhana untuk mendeteksi komponen PC di meja
+public class MejaSensor : MonoBehaviour {
+    public bool isHasMonitor, isHasCPU, isHasKeyboard;
 
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Monitor")) isHasMonitor = true;
+        if (other.CompareTag("CPU")) isHasCPU = true;
+        if (other.CompareTag("Keyboard")) isHasKeyboard = true;
+
+        CheckPCReady();
+    }
+
+    void CheckPCReady() {
+        if (isHasMonitor && isHasCPU && isHasKeyboard) {
+            Debug.Log("PC Siap Digunakan!");
+        }
+    }
+}
